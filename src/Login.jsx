c2438764a78fbad70113c3fbb802a038fbcca8ee
ZofatechIT <zofatechithub@gmail.com>
2025-07-15 15:28:36 +0500

    change

--- src/Login.jsx
+++ src/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
     setLoading(true);
     setError('');
     try {
-      const res = await fetch('http://localhost:5001/api/login', {
+      const res = await fetch('https://attendencemanager-backend.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ employeeId, password }),
